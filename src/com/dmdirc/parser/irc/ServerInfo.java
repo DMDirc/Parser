7b7d1ba7daa5d8b5c4ec0838163d67de039b616c
Chris Smith <chris@dmdirc.com>
2010-01-22 13:28:33 +0000

    Add parser method to update parser with a URI
    Issue 3254

--- src/com/dmdirc/parser/irc/ServerInfo.java
+++ src/com/dmdirc/parser/irc/ServerInfo.java
@@ -27,7 +27,7 @@ import java.net.URISyntaxException;
 
 /**
  * Contains Server information.
- * 
+ *
  * @author Shane Mc Cormack
  * @author Chris Smith
  * @see IRCParser
@@ -47,7 +47,7 @@ public class ServerInfo {
     /** Optional password needed to connect to server (Default: ""). */
     private String password = "";
     /** Is this an ssl-enabled server (Default: false). */
-    private boolean isSSL = false;    
+    private boolean isSSL = false;
     /** Are we using a socks proxy (Default: false). */
     private boolean useSocksProxy = false;
     /** Proxy server to connect to (Default: "127.0.0.1"). */
@@ -60,10 +60,10 @@ public class ServerInfo {
     private String proxyPass = "";
     /** URI used to create this ServerInfo if applicable */
     private URI uri = null;
-    
+
     /** Constructor using Default values. */
     public ServerInfo () { }
-    
+
     /**
      * Constructor using specifed host, port and password, SSL/Proxy must be specifed separately.
      *
@@ -87,7 +87,7 @@ public class ServerInfo {
     public ServerInfo(final URI uri) {
         setURI(uri);
     }
-    
+
     /**
      * Get the URI for this ServerInfo.
      * This will return a new URI based on this ServerInfo.
@@ -134,7 +134,7 @@ public class ServerInfo {
             }
         }
     }
-    
+
     /**
      * Set the URI for this ServerInfo.
      * This will overwrite host/port/password and isSSL.
@@ -159,91 +159,91 @@ public class ServerInfo {
      * @param newValue Value to set to.
      */
     public void setHost(final String newValue) { host = newValue; }
-    
+
     /**
      * Get the hostname.
      *
      * @return Current hostname
      */
     public String getHost() { return host; }
-    
+
     /**
      * Set the port.
      *
      * @param newValue Value to set to.
      */
     public void setPort(final int newValue) { port = newValue; }
-    
+
     /**
      * Get the port.
      *
      * @return Current port
      */
     public int getPort() { return port; }
-    
+
     /**
      * Set the password.
      *
      * @param newValue Value to set to.
      */
     public void setPassword(final String newValue) { password = newValue; }
-    
+
     /**
      * Get the password.
      *
      * @return Current Password
      */
     public String getPassword() { return password; }
-    
+
     /**
      * Set if the server uses ssl.
      *
      * @param newValue true if server uses ssl, else false
      */
     public void setSSL(final boolean newValue) { isSSL = newValue; }
-    
+
     /**
      * Get if the server uses ssl.
      *
      * @return true if server uses ssl, else false
      */
     public boolean getSSL() { return isSSL; }
-    
+
     /**
      * Set if we are connecting via a socks proxy.
      *
      * @param newValue true if we are using socks, else false
      */
     public void setUseSocks(final boolean newValue) { useSocksProxy = newValue; }
-    
+
     /**
      * Get if we are connecting via a socks proxy.
      *
      * @return true if we are using socks, else false
      */
     public boolean getUseSocks() { return useSocksProxy; }
-    
+
     /**
      * Set the Proxy hostname.
      *
      * @param newValue Value to set to.
      */
     public void setProxyHost(final String newValue) { proxyHost = newValue; }
-    
+
     /**
      * Get the Proxy hostname.
      *
      * @return Current Proxy hostname
      */
     public String getProxyHost() { return proxyHost; }
-    
+
     /**
      * Set the Proxy port.
      *
      * @param newValue Value to set to.
      */
     public void setProxyPort(final int newValue) { proxyPort = newValue; }
-    
+
     /**
      * Get the Proxy port.
      *
@@ -257,26 +257,46 @@ public class ServerInfo {
      * @param newValue Value to set to.
      */
     public void setProxyUser(final String newValue) { proxyUser = newValue; }
-    
+
     /**
      * Get the Proxy username.
      *
      * @return Current Proxy username
      */
     public String getProxyUser() { return proxyUser; }
-    
+
     /**
      * Set the Proxy password.
      *
      * @param newValue Value to set to.
      */
     public void setProxyPass(final String newValue) { proxyPass = newValue; }
-    
+
     /**
      * Get the Proxy password.
      *
      * @return Current Proxy password
      */
     public String getProxyPass() { return proxyPass; }
+
+    /**
+     * Retrieves a String describing any channels included in this ServerInfo's
+     * URI.
+     *
+     * @return A channel string for this URI, or null if no URI specified
+     * @since 0.6.3
+     */
+    public String getChannels() {
+        if (uri == null) {
+            return null;
+        }
+
+        String channelString = uri.getPath();
+        if (uri.getRawQuery() != null && !uri.getRawQuery().isEmpty()) { channelString += "?" + uri.getRawQuery(); }
+        if (uri.getRawFragment() != null && !uri.getRawFragment().isEmpty()) { channelString += "#" + uri.getRawFragment(); }
+        if (channelString.startsWith("/")) { channelString = channelString.substring(1); }
+
+        return channelString;
+    }
 }
 
