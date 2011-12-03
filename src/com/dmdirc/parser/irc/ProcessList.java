4ee812d706b349a8b6fb764af1f769ad7989299c
Chris Smith <chris@dmdirc.com>
2011-12-03 20:26:45 +0000

    And finally add a processor for list responses
    Issue: CLIENT-48
    Change-Id: I43aeb1bd963eb5a2c01fa7442681b71a75bbcca2
    Reviewed-on: http://gerrit.dmdirc.com/2202
    Automatic-Compile: DMDirc Build Manager
    Reviewed-by: Greg Holmes <greg@dmdirc.com>

---
+++ src/com/dmdirc/parser/irc/ProcessList.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2006-2011 DMDirc Developers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.parser.irc;
+
+import com.dmdirc.parser.interfaces.callbacks.GroupListEndListener;
+import com.dmdirc.parser.interfaces.callbacks.GroupListEntryListener;
+import com.dmdirc.parser.interfaces.callbacks.GroupListStartListener;
+
+/**
+ * Process a list response.
+ */
+public class ProcessList extends IRCProcessor {
+
+    /**
+     * Create a new instance of the IRCProcessor Object.
+     *
+     * @param parser IRCParser That owns this IRCProcessor
+     * @param manager ProcessingManager that is in charge of this IRCProcessor
+     */
+    protected ProcessList(final IRCParser parser, final ProcessingManager manager) {
+        super(parser, manager);
+    }
+
+    /**
+     * Process a list response.
+     *
+     * @param sParam Type of line to process
+     * @param token IRCTokenised line to process
+     */
+    @Override
+    public void process(final String sParam, final String[] token) {
+        // :port80b.se.quakenet.org 321 MD87 Channel :Users  Name
+        // :port80b.se.quakenet.org 322 MD87 #DMDirc 10 :
+        // :port80b.se.quakenet.org 323 MD87 :End of /LIST
+
+        if ("321".equals(sParam)) {
+            getCallbackManager().getCallback(GroupListStartListener.class)
+                    .onGroupListStart(null, null);
+        } else if ("322".equals(sParam)) {
+            getCallbackManager().getCallback(GroupListEntryListener.class)
+                    .onGroupListEntry(null, null, token[3], Integer.parseInt(token[4]), token[5]);
+        } else if ("323".equals(sParam)) {
+            getCallbackManager().getCallback(GroupListEndListener.class)
+                    .onGroupListEnd(null, null);
+        }
+    }
+
+    /**
+     * What does this IRCProcessor handle.
+     *
+     * @return String[] with the names of the tokens we handle.
+     */
+    @Override
+    public String[] handles() {
+        return new String[]{"321", "322", "323"};
+    }
+}
