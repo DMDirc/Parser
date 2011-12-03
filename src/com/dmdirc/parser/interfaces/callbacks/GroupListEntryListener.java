f7f465aaf7e72f2965ea7e8f29dd7abb562d2da3
Chris Smith <chris@dmdirc.com>
2011-12-03 20:16:02 +0000

    Add interfaces for group listing
    Issue: CLIENT-48
    Change-Id: I6af72c73a4370f5a49315a7a9789a18ea199a888

---
+++ src/com/dmdirc/parser/interfaces/callbacks/GroupListEntryListener.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2006-2011 DMDirc Developers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.parser.interfaces.callbacks;
+
+import com.dmdirc.parser.interfaces.Parser;
+import java.util.Date;
+
+/**
+ * Callback interface for when the parser receives an entry during a group
+ * list response.
+ */
+public interface GroupListEntryListener extends CallbackInterface {
+
+    /**
+     * Called when a single group entry has been received.
+     *
+     * @param parser The parser raising the event
+     * @param date The date at which the event occurred
+     * @param name The name of the channel
+     * @param users The number of users in the channel
+     * @param topic The topic of the channel
+     */
+    void onGroupListEntry(Parser parser, Date date, String name, int users,
+            String topic);
+
+}
