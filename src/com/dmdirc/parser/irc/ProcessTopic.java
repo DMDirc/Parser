DMDirc/Parser

commit 6864a35fa2ca95b7dfbb1137a620e6fb33512c07

Java 7 conversions
Change-Id: Id834157281c02302bfe6f71201042712144b22ff
Reviewed-on: http://gerrit.dmdirc.com/2894
Automatic-Compile: DMDirc Build Manager
Reviewed-by: Chris Smith <chris@dmdirc.com>

src/com/dmdirc/parser/irc/ProcessTopic.java
@@ -49,44 +49,43 @@ public class ProcessTopic extends IRCProcessor {
     @Override
     public void process(final String sParam, final String[] token) {
         IRCChannelInfo iChannel;
-        if (sParam.equals("332")) {
-            iChannel = getChannel(token[3]);
-            if (iChannel == null) {
-                return;
-            }
-            iChannel.setInternalTopic(token[token.length - 1]);
-        } else if (sParam.equals("333")) {
-            if (token.length > 3) {
+        switch (sParam) {
+            case "332":
                 iChannel = getChannel(token[3]);
                 if (iChannel == null) {
                     return;
-                }
-                if (token.length > 4) {
-                    iChannel.setTopicUser(token[4]);
-                    if (token.length > 5) {
-                        iChannel.setTopicTime(Long.parseLong(token[5]));
+                }   iChannel.setInternalTopic(token[token.length - 1]);
+                break;
+            case "333":
+                if (token.length > 3) {
+                    iChannel = getChannel(token[3]);
+                    if (iChannel == null) {
+                        return;
                     }
-                }
-                callChannelTopic(iChannel, true);
-            }
-        } else {
-            if (IRCParser.ALWAYS_UPDATECLIENT) {
-                final IRCClientInfo iClient = getClientInfo(token[0]);
-                if (iClient != null && iClient.getHostname().isEmpty()) {
-                    iClient.setUserBits(token[0], false);
-                }
-            }
-            iChannel = getChannel(token[2]);
+                    if (token.length > 4) {
+                        iChannel.setTopicUser(token[4]);
+                        if (token.length > 5) {
+                            iChannel.setTopicTime(Long.parseLong(token[5]));
+                        }
+                    }
+                    callChannelTopic(iChannel, true);
+                }   break;
+            default:
+                if (IRCParser.ALWAYS_UPDATECLIENT) {
+                    final IRCClientInfo iClient = getClientInfo(token[0]);
+                    if (iClient != null && iClient.getHostname().isEmpty()) {
+                        iClient.setUserBits(token[0], false);
+                    }
+                }   iChannel = getChannel(token[2]);
             if (iChannel == null) {
                 return;
-            }
-            iChannel.setTopicTime(System.currentTimeMillis() / 1000);
+            }   iChannel.setTopicTime(System.currentTimeMillis() / 1000);
             if (token[0].charAt(0) == ':') {
                 token[0] = token[0].substring(1);
-            }
-            iChannel.setTopicUser(token[0]);
+            }   iChannel.setTopicUser(token[0]);
             iChannel.setInternalTopic(token[token.length - 1]);
-            callChannelTopic(iChannel, false);
+                callChannelTopic(iChannel, false);
+                break;
         }
     }
 

src/com/dmdirc/parser/common/BaseChannelClientInfo.java
@@ -35,7 +35,7 @@ import java.util.Map;
 public abstract class BaseChannelClientInfo implements ChannelClientInfo {
 
     /** A map for random data associated with the client to be stored in. */
-    private final Map<Object, Object> map = new HashMap<Object, Object>();
+    private final Map<Object, Object> map = new HashMap<>();
 
     /** The channel that the client is associated with. */
     private final ChannelInfo channel;

src/com/dmdirc/parser/common/BaseChannelInfo.java
@@ -39,10 +39,10 @@ public abstract class BaseChannelInfo implements ChannelInfo {
     private final Parser parser;
 
     /** A map for random data associated with the client to be stored in. */
-    private final Map<Object, Object> map = new HashMap<Object, Object>();
+    private final Map<Object, Object> map = new HashMap<>();
 
     /** The clients in this channel. */
-    private final Map<String, ChannelClientInfo> clients = new HashMap<String, ChannelClientInfo>();
+    private final Map<String, ChannelClientInfo> clients = new HashMap<>();
 
     /** The name of this channel. */
     private final String name;

src/com/dmdirc/parser/common/BaseClientInfo.java
@@ -37,7 +37,7 @@ public abstract class BaseClientInfo implements ClientInfo {
     private final Parser parser;
 
     /** A map for random data associated with the client to be stored in. */
-    private final Map<Object, Object> map = new HashMap<Object, Object>();
+    private final Map<Object, Object> map = new HashMap<>();
 
     /** The user's details. */
     private String nick, user, host, realname, account = null;

src/com/dmdirc/parser/common/CallbackManager.java
@@ -110,7 +110,7 @@ public class CallbackManager {
 
     /** Hashtable used to store the different types of callback known. */
     private final Map<Class<? extends CallbackInterface>, CallbackObject> callbackHash
-            = new HashMap<Class<? extends CallbackInterface>, CallbackObject>();
+            = new HashMap<>();
 
     /** A map of implementations to use for parser interfaces. */
     private final Map<Class<?>, Class<?>> implementationMap;

src/com/dmdirc/parser/common/CallbackObject.java
@@ -19,7 +19,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-
 package com.dmdirc.parser.common;
 
 import com.dmdirc.parser.interfaces.FakableArgument;
@@ -47,8 +46,8 @@ public class CallbackObject {
     protected final Class<? extends CallbackInterface> type;
 
     /** Arraylist for storing callback information related to the callback. */
-    protected final List<CallbackInterface> callbackInfo =
-            new CopyOnWriteArrayList<CallbackInterface>();
+    protected final List<CallbackInterface> callbackInfo
+            = new CopyOnWriteArrayList<>();
 
     /** Reference to the Parser that owns this callback. */
     protected Parser myParser;
@@ -168,7 +167,8 @@ public class CallbackObject {
         for (CallbackInterface iface : callbackInfo) {
             try {
                 type.getMethods()[0].invoke(iface, newArgs);
-            } catch (Exception e) {
+            } catch (SecurityException | IllegalAccessException |
+                    IllegalArgumentException | InvocationTargetException e) {
                 if (getType().equals(ErrorInfoListener.class)) {
                     System.out.printf("Exception in onError Callback. [%s]\n", e.getMessage());
                     e.printStackTrace();
@@ -187,10 +187,10 @@ public class CallbackObject {
     }
 
     /**
-     * Replaces all null entries in the specified array with fake values,
-     * if the corresponding parameter of this callback's type is marked with
-     * the {@link FakableArgument} annotation. The fake classes are constructed
-     * by using parameters designated {@link FakableSource}.
+     * Replaces all null entries in the specified array with fake values, if the
+     * corresponding parameter of this callback's type is marked with the
+     * {@link FakableArgument} annotation. The fake classes are constructed by
+     * using parameters designated {@link FakableSource}.
      *
      * @param args The arguments to be faked
      */
@@ -217,15 +217,15 @@ public class CallbackObject {
      * infinite recursion in cases of cyclic dependencies. If recursion fails,
      * the constructor is skipped.
      *
-     * If the created object has a <code>setFake(boolean)</code> method, it
-     * is automatically invoked with an argument of <code>true</code>.
+     * If the created object has a <code>setFake(boolean)</code> method, it is
+     * automatically invoked with an argument of <code>true</code>.
      *
      * @param args The arguments array to use for sources
      * @param target The class that should be constructed
      * @return An instance of the target class, or null on failure
      */
     protected Object getFakeArg(final Object[] args, final Class<?> target) {
-        final Map<Class<?>, Object> sources = new HashMap<Class<?>, Object>();
+        final Map<Class<?>, Object> sources = new HashMap<>();
         int i = 0;
 
         for (Annotation[] anns : type.getMethods()[0].getParameterAnnotations()) {
@@ -285,13 +285,8 @@ public class CallbackObject {
                     }
 
                     return instance;
-                } catch (InstantiationException ex) {
-                    // Do nothing
-                } catch (IllegalAccessException ex) {
-                    // Do nothing
-                } catch (IllegalArgumentException ex) {
-                    // Do nothing
-                } catch (InvocationTargetException ex) {
+                } catch (InstantiationException | IllegalAccessException |
+                        IllegalArgumentException | InvocationTargetException ex) {
                     // Do nothing
                 }
             }
@@ -301,12 +296,12 @@ public class CallbackObject {
     }
 
     /**
-     * Returns the concrete class which should be instansiated if this
-     * callback object desires an instance of the specified type. Generally,
-     * this will translate the common interfaces to specific parser-dependent
+     * Returns the concrete class which should be instantiated if this callback
+     * object desires an instance of the specified type. Generally, this will
+     * translate the common interfaces to specific parser-dependent
      * implementations.
      *
-     * @param type The type to be instansiated
+     * @param type The type to be instantiated
      * @return A concrete implementation of that type
      */
     protected Class<?> getImplementation(final Class<?> type) {

src/com/dmdirc/parser/common/CallbackObjectSpecific.java
@@ -39,7 +39,7 @@ public class CallbackObjectSpecific extends CallbackObject {
 
     /** Hashtable for storing specific information for callback. */
     protected final Map<CallbackInterface, String> specificData
-            = new HashMap<CallbackInterface, String>();
+            = new HashMap<>();
 
     /**
      * Create a new instance of the Callback Object.
@@ -147,7 +147,7 @@ public class CallbackObjectSpecific extends CallbackObject {
 
         createFakeArgs(newArgs);
 
-        for (CallbackInterface iface : new ArrayList<CallbackInterface>(callbackInfo)) {
+        for (CallbackInterface iface : new ArrayList<>(callbackInfo)) {
             if (type.isAnnotationPresent(SpecificCallback.class)
                     && ((args[0] instanceof ClientInfo
                     && !isValidUser(iface, ((ClientInfo) args[0]).getHostname()))
@@ -162,7 +162,7 @@ public class CallbackObjectSpecific extends CallbackObject {
 
             try {
                 type.getMethods()[0].invoke(iface, newArgs);
-            } catch (Exception e) {
+            } catch (ReflectiveOperationException e) {
                 final ParserError ei = new ParserError(ParserError.ERROR_ERROR,
                         "Exception in callback (" + e.getMessage() + ")",
                         myParser.getLastLine());

src/com/dmdirc/parser/common/IgnoreList.java
@@ -28,13 +28,11 @@ import java.util.regex.PatternSyntaxException;
 
 /**
  * Parser Ignore list.
- *
- * @author Shane Mc Cormack
  */
 public class IgnoreList {
 
     /** Arraylist storing ignore patterns. */
-    protected final List<String> ignoreInfo = new ArrayList<String>();
+    protected final List<String> ignoreInfo = new ArrayList<>();
 
     /**
      * Creates a new instance of RegexStringList.
@@ -193,7 +191,7 @@ public class IgnoreList {
      * @return All expressions in this ignore list
      */
     public List<String> getRegexList() {
-        return new ArrayList<String>(ignoreInfo);
+        return new ArrayList<>(ignoreInfo);
     }
 
     /**
@@ -203,7 +201,7 @@ public class IgnoreList {
      * @throws UnsupportedOperationException if an expression can't be converted
      */
     public List<String> getSimpleList() throws UnsupportedOperationException {
-        final List<String> res = new ArrayList<String>();
+        final List<String> res = new ArrayList<>();
 
         for (String regex : ignoreInfo) {
             res.add(regexToSimple(regex));

commit 8cad247b77eaba91dad83118aefb55456f6578d3

Fix formatting errors introduced in 6864a35fa2ca95b7dfbb1137a620e6fb33512c07
Change-Id: I7ed80c3e353e8f68f97928555e4f6e114a03b755

src/com/dmdirc/parser/irc/ProcessTopic.java
@@ -76,14 +76,17 @@ public class ProcessTopic extends IRCProcessor {
                     if (iClient != null && iClient.getHostname().isEmpty()) {
                         iClient.setUserBits(token[0], false);
                     }
-                }   iChannel = getChannel(token[2]);
-            if (iChannel == null) {
-                return;
-            }   iChannel.setTopicTime(System.currentTimeMillis() / 1000);
-            if (token[0].charAt(0) == ':') {
-                token[0] = token[0].substring(1);
-            }   iChannel.setTopicUser(token[0]);
-            iChannel.setInternalTopic(token[token.length - 1]);
+                }
+                iChannel = getChannel(token[2]);
+                if (iChannel == null) {
+                    return;
+                }
+                iChannel.setTopicTime(System.currentTimeMillis() / 1000);
+                if (token[0].charAt(0) == ':') {
+                    token[0] = token[0].substring(1);
+                }
+                iChannel.setTopicUser(token[0]);
+                iChannel.setInternalTopic(token[token.length - 1]);
                 callChannelTopic(iChannel, false);
                 break;
         }